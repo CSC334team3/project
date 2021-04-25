4b8ba7ac9ae11e6640fe21d5b79eb99bc13e5c1e
thilton203 <hiltont1@southernct.edu>
2021-04-25 12:03:56 -0400

    grade tab implemented (#1)
    * grade tab implemented
    
    * Delete test.txt
    
    * updated path to style sheet
    
    * deleted test.txt

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//Sets first course as default selection... but haven't figured out how to deselect afterwards
+// window.onload = function () {
+//     let defaultCourse = document.getElementById("course1-gradeList");
+//     defaultCourse.style.borderRight = "none";
+//     defaultCourse.style.borderTopRightRadius = "0";
+// };
